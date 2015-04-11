df27e8638752aad275391586dcf62f791e9f9b9b
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-04-11 17:38:41 -0400

    Front page beta

---
+++ source/assets/javascripts/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', 'thirdPage'],
+    // sectionsColor: ['#8FB98B', '#DE564B', '#EAE1C0'],
+    slidesNavigation: true,
+  });
+});
