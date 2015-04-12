4134975e10981550ad3af704a7ab155113858748
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-04-11 22:09:56 -0400

    Search page beta

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,7 +1,32 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors: ['firstPage', 'secondPage', 'thirdPage'],
-    // sectionsColor: ['#8FB98B', '#DE564B', '#EAE1C0'],
     slidesNavigation: true,
   });
+
+  // $('#main-box').on('click', 'a', function(e) {
+  //   e.preventDefault();
+  //   alert('hi');
+  // })
 });
+
+
+// ---------------------------------------------------------------------------
+// Imput
+// -----
+
+window.onload = function(){
+  var start = document.getElementById("start-business");
+  var input = document.getElementById("input");
+
+  // setTimeout(function() {
+  //   TweenLite.to(start, 2.5, {
+  //     scale: 0});
+  // }, 500);
+
+  // input.onclick = function() {
+  //   TweenLite.to(bar, 2, {width:"100%"});
+  // }
+
+  
+}
