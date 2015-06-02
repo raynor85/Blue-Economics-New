13271de7a411eece1acd91982bded0ca8a780ab8
raynor85 <raynor85@gmail.com>
2015-06-02 02:16:32 -0400

    Jorge code alignment

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-  $('#fullpage').fullpage({
-    anchors: ['firstPage', 'secondPage', 'thirdPage'],
-    slidesNavigation: true,
-  });
+  if(screen.width < 480) {
+    // $('#fullpage').fullpage({
+    //   anchors: ['firstPage', 'secondPage', 'thirdPage'],
+    //   slidesNavigation: true,
+    //   continuousVertical: false,
+    //   setAutoScrolling: false
+    // });
+    return;
+  } 
+  else {
+    $('#fullpage').fullpage({
+      anchors: ['firstPage', 'secondPage', 'thirdPage'],
+      slidesNavigation: true,
+      continuousVertical: false
+    });
+  }
 });
+
