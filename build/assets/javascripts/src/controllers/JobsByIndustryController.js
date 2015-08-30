c2925203ac6b854f0516d9b929cd3cf1d0ed550d
john <john@localhost>
2015-08-30 15:33:34 -0400

    Fixed merge conflict in JobsByIndustryController

--- build/assets/javascripts/src/controllers/JobsByIndustryController.js
+++ build/assets/javascripts/src/controllers/JobsByIndustryController.js
@@ -13,6 +13,15 @@ function JobsByIndustryController($state, jobs, blueEconomics) {
 
     this.jobs = jobs;
     this.selectJob = selectJob;
+	this.getSalaryForJob = function() { return salaryForJob('Automotive Body and Glass Repairers'); };
+
+
+    function salaryForJob(jobName) {
+    	blueEconomics.jobDescriptions.get(jobName)
+    		.then(function(data) {
+    			console.log(data);
+    		});
+    }
 
     function selectJob(job) {
 
