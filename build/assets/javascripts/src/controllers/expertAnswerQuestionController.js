3f1f2beda758f354b20cb51b711ae7fa1a051124
harihpr <rharihprasad@gmail.com>
2015-10-27 21:27:59 -0400

    Added new four new pages and respective views and controllers

---
+++ build/assets/javascripts/src/controllers/expertAnswerQuestionController.js
@@ -0,0 +1,19 @@
+/**
+ * Created by hari on 10/20/15.
+ */
+
+'use strict';
+
+require('../scss/expertPages.scss');
+
+/**
+ * Controller for the initial page.
+ * @param $scope
+ * @constructor
+ * @ngInject
+ */
+function expertAnswerQuestionController($scope) {
+
+}
+
+module.exports = expertAnswerQuestionController;
